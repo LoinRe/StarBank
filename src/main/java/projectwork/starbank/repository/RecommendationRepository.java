1956e0786af1fc3025ec6e0be003747b25403de8
Drachlo <deuse.sec@gmail.com>
2025-04-20 22:31:10 +0300

    Task-2-Kirill +CacheRepository +CacheRepositoryTest

--- src/main/java/projectwork/starbank/repository/RecommendationRepository.java
+++ src/main/java/projectwork/starbank/repository/RecommendationRepository.java
@@ -1,6 +1,8 @@
 package projectwork.starbank.repository;
 
+
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,51 +15,56 @@ public class RecommendationRepository {
     }
 
     // Проверка: есть ли у пользователя хотя бы один продукт указанного типа
+    @Cacheable(value = "userOf", key = "#userId + '-' + #productType")
     public boolean userHasProductType(String userId, String productType) {
         String sql = """
-            SELECT COUNT(*) FROM transactions t
-            JOIN products p ON t.product_id = p.id
-            WHERE t.user_id = ? AND p.type = ?
-        """;
+                    SELECT COUNT(*) FROM transactions t
+                    JOIN products p ON t.product_id = p.id
+                    WHERE t.user_id = ? AND p.type = ?
+                """;
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, userId, productType);
         return count != null && count > 0;
     }
 
     // Сумма всех пополнений по продуктам определённого типа
+    @Cacheable(value = "sumByType", key = "#userId + '-' + #productType + '-' + #transactionType")
     public double getSumDepositsByProductType(String userId, String productType) {
         String sql = """
-            SELECT COALESCE(SUM(t.amount), 0) FROM transactions t
-            JOIN products p ON t.product_id = p.id
-            WHERE t.user_id = ? AND p.type = ? AND t.type = 'DEPOSIT'
-        """;
+                    SELECT COALESCE(SUM(t.amount), 0) FROM transactions t
+                    JOIN products p ON t.product_id = p.id
+                    WHERE t.user_id = ? AND p.type = ? AND t.type = 'DEPOSIT'
+                """;
         return jdbcTemplate.queryForObject(sql, Double.class, userId, productType);
     }
 
     // Сумма всех трат по продуктам определённого типа
+    @Cacheable(value = "sumByType", key = "#userId + '-' + #productType + '-' + #transactionType")
     public double getSumWithdrawalsByProductType(String userId, String productType) {
         String sql = """
-            SELECT COALESCE(SUM(t.amount), 0) FROM transactions t
-            JOIN products p ON t.product_id = p.id
-            WHERE t.user_id = ? AND p.type = ? AND t.type = 'WITHDRAW'
-        """;
+                    SELECT COALESCE(SUM(t.amount), 0) FROM transactions t
+                    JOIN products p ON t.product_id = p.id
+                    WHERE t.user_id = ? AND p.type = ? AND t.type = 'WITHDRAW'
+                """;
         return jdbcTemplate.queryForObject(sql, Double.class, userId, productType);
     }
+
+    @Cacheable(value = "userTransactionCount", key = "#userId + '-' + #productType")
     public int userTransactionCount(String userId, String productType) {
         String sql = """
-        SELECT COUNT(*) FROM transactions t 
-        JOIN products p ON t.product_id = p.id 
-        WHERE t.user_id = ? AND p.type = ? 
-    """;
+                    SELECT COUNT(*) FROM transactions t 
+                    JOIN products p ON t.product_id = p.id 
+                    WHERE t.user_id = ? AND p.type = ? W
+                """;
         return jdbcTemplate.queryForObject(sql, Integer.class, userId, productType);
     }
 
-
+    @Cacheable(value = "sumByType", key = "#userId + '-' + #productType + '-' + #transactionType")
     public double getSumByTransactionType(String userId, String productType, String transactionType) {
         String sql = """
-        SELECT COALESCE(SUM(t.amount), 0) FROM transactions t 
-        JOIN products p ON t.product_id = p.id 
-        WHERE t.user_id = ? AND p.type = ? AND t.type = ? 
-    """;
+                    SELECT COALESCE(SUM(t.amount), 0) FROM transactions t 
+                    JOIN products p ON t.product_id = p.id 
+                    WHERE t.user_id = ? AND p.type = ? AND t.type = ? 
+                """;
         return jdbcTemplate.queryForObject(sql, Double.class, userId, productType, transactionType);
     }
 }
